e5c4d9d406a2dd518784ea9155b47accd8b41052
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-12-09 18:08:32 -0600

    fix routes, route, navbar

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,16 +1,14 @@
-import {Link, Route, Routes} from "react-router-dom";
+import {Route, Routes, Router, } from "react-router-dom";
 import NavBar from './NavBar'
 import Home from './Home'
 
 function App() {
     return (
         <div>
-            <h1>1248 Learning</h1>
             <NavBar />
             <Routes>
-                <Route exact path='/'>
-                    <Home />
-                </Route>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={'404 Not Found'} />
             </Routes>
         </div>
     )
--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -4,8 +4,9 @@ import { NavLink } from "react-router-dom";
 function NavBar(){
     return (
         <div>
+            <h1>1248 Learning</h1>
             <nav>
-                <NavLink className='navigation-links' exact to='/'>Home</NavLink>
+                <NavLink className='navigation-links' to='/'>Home</NavLink>
             </nav>
         </div>
     )
