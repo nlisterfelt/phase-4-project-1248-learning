a74f77b2d78f65b31b83ddad2f787ab76f5ae6ca
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-12-10 11:27:46 -0600

    add a login button

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar(){
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    function handleLogin(){
+        setIsLoggedIn(!isLoggedIn)
+    }
+
     return (
         <div>
             <h1>1248 Learning</h1>
             <nav>
                 <NavLink className='navigation-links' to='/'>Home</NavLink>
+                <button style={{float: 'right'}} onClick={handleLogin}>{isLoggedIn ? 'Logout' : 'Login'}</button>
             </nav>
         </div>
     )
