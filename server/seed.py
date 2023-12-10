45bc7da7fc6ccd91e85fc9073f54ebfbf7a9696c
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-12-10 13:42:59 -0600

    add seed data for users table

--- server/seed.py
+++ server/seed.py
@@ -4,14 +4,28 @@
 from random import randint, choice as rc
 
 # Remote library imports
-from faker import Faker
+#from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User
 
 if __name__ == '__main__':
-    fake = Faker()
+    #fake = Faker()
     with app.app_context():
         print("Starting seed...")
-        # Seed code goes here!
+        User.query.delete()
+        print("Creating users...")
+        users = []
+        usernames = []
+        for i in range(10):
+            username = f'name{i}'
+            usernames.append(username)
+            user = User(
+                username = username,
+            )
+            user.password_hash = user.username + 'password'
+            users.append(user)
+        db.session.add_all(users)
+        db.session.commit()
+        print("Complete.")
