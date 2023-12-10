a270effb6a5d97c366b2cff6a7cb4f505857ad13
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-12-10 14:51:17 -0600

    add decks table to models.py

--- server/models.py
+++ server/models.py
@@ -5,20 +5,37 @@ from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
-    serialize_rules = ('-_password_hash',)
+    serialize_rules = ('-_password_hash', '-decks.user')
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
+    username = db.Column(db.String, nullable=False)
     _password_hash = db.Column(db.String)
+
+    decks = db.relationship('Deck', backref='user')
 
     @hybrid_property
     def password_hash(self):
-        raise AttributeError('Password hasses may not be viewed.')
+        raise AttributeError('Password hashes may not be viewed.')
 
     @password_hash.setter
     def password_hash(self):
         password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
         self._password_hash = password_hash.decode('utf-8')
 
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
     def __repr__(self):
         return f'<User {self.username}>'
+
+class Deck(db.Model, SerializerMixin):
+    __tablename__ = 'decks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f'<Deck {id}: {name}'
+
+
