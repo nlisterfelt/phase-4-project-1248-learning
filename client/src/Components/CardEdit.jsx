8fbde6f13ec59e4d67902692b89e8e1c70642e0a
Nicole Listerfelt <nlisterfelt@gmail.com>
2024-01-21 16:47:58 -0600

    add decks and reviews to CardEdit

--- client/src/Components/CardEdit.jsx
+++ client/src/Components/CardEdit.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
 const CardEdit = ({card}) => {
+    const deckList = card.decks.map(deck=><li key={deck.id} id={deck.id}>{deck.id}</li>)
+    const reviewList = card.reviews.map(review=><li key={review.id} id={review.id}>Deck: {review.deck_id}, Session: {review.session}, Level: {review.level} <button onClick={e=>handleReviewEdit(review)}>Edit</button>  <button onClick={e=>handleReviewDelete(review)}>X</button></li>)
+
+    function handleReviewEdit(review){
+        console.log(review)
+    }
+    function handleReviewDelete(review){
+        console.log(review)
+    }
     return (
         <div>
             {card.front_title}
-            
+            <p style={{color: 'red'}}>When the last review or the deck with the last review is deleted, this card will be deleted as well.</p>
+            <h4>Decks <button>Add</button></h4>
+            <ul>{deckList}</ul>
+            <h4>Reviews</h4>
+            <ul>{reviewList}</ul>
         </div>
     )
 }
